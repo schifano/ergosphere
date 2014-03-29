2f897e95d03fcb55f05639016ff5f64ff9ce3232
Rachel Schifano <rschifa1@gmail.com>
2014-03-29 00:40:26 -0500

    Created test IBAction functions that bind alert messages to buttons, also differentiate action between iPad and iPhone

---
+++ Ergosphere/CoursesViewController.m
@@ -0,0 +1,13 @@
+//
+//  CoursesViewController.m
+//  Ergosphere
+//
+//  Created by Rachel Schifano on 3/29/14.
+//  Copyright (c) 2014 Rachel Schifano. All rights reserved.
+//
+
+#import "CoursesViewController.h"
+
+@implementation CoursesViewController
+
+@end
--- Ergosphere/ViewController.m
+++ Ergosphere/ViewController.m
@@ -30,4 +30,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+// TEST TEST TEST
+-(IBAction)showMeow:(id)sender{
+    
+    UIAlertView *alert =[[UIAlertView alloc] initWithTitle:@"I love Michal" message:@"Hug Hug Kitty Meow" delegate:self cancelButtonTitle:@"yah okie" otherButtonTitles:nil, nil];
+    [alert show];
+}
+// TEST TEST TEST
+- (IBAction)play:(id)sender {
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        // For iPhone
+        UIAlertView *playAlert = [[UIAlertView alloc] initWithTitle:@"New Game" message:@"Start Playing..." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [playAlert show];
+        
+    } else {
+        // For iPad
+        UIAlertView *playAlert = [[UIAlertView alloc] initWithTitle:@"Game Over" message:@"sry u lost the game" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [playAlert show];
+    }
+}
+
 @end
