ac3e8057fa86467206adc3c5488cf14500e55d15
Rachel Schifano <rschifa1@gmail.com>
2014-04-02 20:28:52 -0500

    Added Table View to iPad storyboard

---
+++ Ergosphere/TopicViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopicViewController.h
+//  Ergosphere
+//
+//  Created by Rachel Schifano on 4/2/14.
+//  Copyright (c) 2014 Rachel Schifano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicViewController : UIViewController
+
+@end
